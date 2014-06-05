fc5828805fcd5a9dac8912151ea438e08faf7e47
Thomas Levine <_@thomaslevine.com>
2014-06-05 20:08:55 +0000

    write it

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='austin-data-catalog',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Archive the /api/views endpoint on Austin\'s data catalog.',
+      url='https://github.com/tlevine/austin-data-catalog',
+      packages=['pluplusch'],
+      install_requires = [
+          'pluplusch>=0.0.6',
+      ],
+      scripts = ['austin'],
+      version='0.0.1',
+      license='AGPL',
+)
